76193046429e9e86dfc3e69704e0e7fb34e8ac6d
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 19:50:17 -0700

    complete omniauth fb setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :comments
   resources :realtors
   resources :tasks
   resources :rooms
   root 'rooms#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
