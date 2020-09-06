21b59ababcf582d20a4c751be46368e957f19c20
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 22:30:25 -0700

    building create rooms method

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,19 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = current_user.rooms.new(room_params)
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render new_room_path
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description, :features)
+  end
 end
