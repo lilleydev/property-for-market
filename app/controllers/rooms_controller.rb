f923200e59d5dcc1ead44b3dfd01afe8fd2e3c19
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 22:40:40 -0700

    rooms index page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,12 +4,17 @@ class RoomsController < ApplicationController
   end
 
   def create
+    # binding.pry
     @room = current_user.rooms.new(room_params)
     if @room.save
       redirect_to room_path(@room)
     else
       render new_room_path
     end
+  end
+
+  def index
+    @rooms = Room.all
   end
 
   private
