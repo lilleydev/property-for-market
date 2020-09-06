4a14b70f2027c01474d7509570a6832deadc61d8
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 19:39:19 -0700

    run omniauth migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_023054) do
+ActiveRecord::Schema.define(version: 2020_09_06_023858) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "room_id", null: false
@@ -57,8 +57,12 @@ ActiveRecord::Schema.define(version: 2020_09_06_023054) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
   add_foreign_key "comments", "rooms"
