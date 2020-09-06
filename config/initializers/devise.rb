fd567fec0882f058d2f45c4e162aa6a1209be835
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-06 09:10:45 -0700

    add all rooms to navbar

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'public_profile,email',
+                                                                          info_fields: 'email,first_name,last_name,gender,birthday,location,picture'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
