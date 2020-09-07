8ba9f6706f0219cd6225dc94d044cdf90cd3750f
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-07 09:29:18 -0700

    set up private tasks methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:params).permit(:name, :description, :completed)
+  end
 end
