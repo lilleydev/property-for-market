85581bc42a11321b3cea429f05b1c6dcd7fe1145
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-07 09:50:07 -0700

    tasks create method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,24 @@
 class TasksController < ApplicationController
   before_action :get_room
   def index
-    @tasks = Task.all
+    @tasks = @room.tasks
   end
 
   def new
-    @task = Task.new
+    @task = @room.tasks.build
+  end
+
+  def create
+    @task = @room.tasks.build(task_params)
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to room_tasks_path(@room), notice: 'Task was successfully created.' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+   end
+    end
   end
 
   private
