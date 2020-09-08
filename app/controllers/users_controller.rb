f2978cfec03f125e889458ccfbcf6649e66208f5
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-07 20:30:28 -0700

    route and view for user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
   def show; end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :realtors
-
+  resources :users, only: [:show]
   root 'rooms#index'
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' },
                      controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
