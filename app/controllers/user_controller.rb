f814900a0b4b948770f93ee3ea426ab9e76a4588
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-07 20:21:13 -0700

    add user controller and #show

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,10 @@
+class UserController < ApplicationController
+  before_action :set_user, only: %i[show]
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find_by_id(params[:id])
+  end
+end
