cdf24a410ea1a1e43415a272165283b3c4843f0e
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-09 10:37:12 -0700

    set private methods comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,18 @@ class CommentsController < ApplicationController
   def new
     @comment = @realtor.comments.build
   end
+
+  private
+
+  def set_comment
+    @comment = @realtor.comments.find(params[:id])
+  end
+
+  def get_realtor
+    @realtor = realtor.find(params[:realtor_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:name, :description)
+  end
 end
