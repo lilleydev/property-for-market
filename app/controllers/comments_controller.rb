0f4cfd204bfc3cc54ad7603d089a44d12c0626e8
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-09 11:28:15 -0700

    fix typo replace realtor with room

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,18 @@
 class CommentsController < ApplicationController
   def index
-    @comments = @realtor.comments
+    @comments = @room.comments
   end
 
   def new
-    @comment = @realtor.comments.build
+    @comment = @room.comments.build
   end
 
   def create
-    @comment = @realtor.comments.build(comment_params)
+    @comment = @room.comments.build(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to realtor_path(@realtor), notice: 'Comment was successfully created.' }
-        format.json { render realtor_path(@realtor), status: :created, location: @comment }
+        format.html { redirect_to room_path(@room), notice: 'Comment was successfully created.' }
+        format.json { render room_path(@room), status: :created, location: @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -23,11 +23,11 @@ class CommentsController < ApplicationController
   private
 
   def set_comment
-    @comment = @realtor.comments.find(params[:id])
+    @comment = @room.comments.find(params[:id])
   end
 
-  def get_realtor
-    @realtor = realtor.find(params[:realtor_id])
+  def get_room
+    @room = room.find(params[:room_id])
   end
 
   def comment_params
