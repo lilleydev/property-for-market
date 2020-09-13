23e813e5f0a1b16f5199037ffe27082b380b9dc4
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-13 09:03:47 -0700

    working on delete funtion to cancel volunteered task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :room
   delegate :user, to: :room
-  has_many :user_tasks
+  has_many :user_tasks, dependent: :destroy
   has_many :helpers, through: :user_tasks, source: :user
   # scope :less_than_3_helpers, -> { joins(:user_tasks).group('tasks.id').having('count(task_id) < 2') }
   scope :needs_help, -> { where(needs_help: true) }
