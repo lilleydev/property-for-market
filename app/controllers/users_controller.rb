fabbd9fb30237b054e6ee2e725cb0cc20effa0e2
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-14 13:42:23 -0700

    delete hashed out code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: %i[show]
   def show
     @user = User.find_by_id(params[:id])
-  end
-
-  private
-
-  def set_user
-    binding.pry
-    @user = User.find(params[:id])
   end
 end
