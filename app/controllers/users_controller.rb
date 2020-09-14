b2e82d320b990ed97a8a6b8655ae50b2e984a350
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-14 13:55:15 -0700

    add authorization to only view users own rooms and profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
+
+    if current_user != @user
+      redirect_to root_path, notice: 'Sorry, but you are only allowed to view your own profile page.'
+  end
   end
 end
