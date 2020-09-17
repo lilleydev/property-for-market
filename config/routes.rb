9bab86a5dd5af9b0e15552401ed0dce7be10a7f3
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-17 14:44:10 -0700

    delete tasks route and add nested index tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,17 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' },
                      controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  # devise_for :users, path_prefix: 'd'
-  resources :users, only: [:show]
+  resources :users, only: %i[show index]
 
-  # match 'users/:id', to: 'users#show', via: 'get'
   root 'rooms#index'
 
   resources :rooms do
-    resources :tasks, except: %i[show], shallow: true do
+    resources :tasks, shallow: true do
       post '/user_tasks', to: 'user_tasks#create', as: 'volunteer'
       get '/user_tasks', to: 'user_tasks#new', as: 'help'
       delete '/user_tasks', to: 'user_tasks#destroy', as: 'cancel'
     end
   end
 
-  resources :tasks, only: [:index]
-  post '/tasks', to: 'tasks#complete', as: 'complete'
   get '/needs_help', to: 'needs_help#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
