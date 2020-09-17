dbdff482aa9e4704b0d12a04ab4b0cecac9c5eaa
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-17 15:00:10 -0700

    change redirect to routes

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -7,13 +7,13 @@ class UserTasksController < ApplicationController
   def create
     task = Task.find_by(id: params[:task_id])
     task.user_tasks.build(user: current_user)
-    redirect_to tasks_path if task.save
+    redirect_to user_path(current_user) if task.save
   end
 
   def destroy
     task = Task.find_by(id: params[:task_id])
     task.helpers.delete(current_user)
 
-    redirect_to tasks_path if task.save
+    redirect_to user_path(current_user) if task.save
   end
 end
