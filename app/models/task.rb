d5feec81876d9051e3eae3784c62135a8fd0fb15
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-23 13:53:57 -0700

    delete commented out code

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,25 +3,8 @@ class Task < ApplicationRecord
   delegate :user, to: :room
   has_many :user_tasks, dependent: :destroy
   has_many :helpers, through: :user_tasks, source: :user
-  # scope :less_than_3_helpers, -> { joins(:user_tasks).group('tasks.id').having('count(task_id) < 2') }
   scope :needs_help, -> { where(needs_help: true) }
 
   validates :name, presence: true
   validates :description, presence: true
-  # needs_help.select do |t|
-  #   t.helpers.count < 2
-  # end
-
-  STATUS = {
-    incomplete: 0,
-    complete: 1
-  }.freeze
-
-  def complete?
-    status == STATUS[:complete]
-  end
-
-  def incomplete?
-    status == STATUS[:incomplete]
-  end
 end
