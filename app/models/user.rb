edaf3c9e705021060abc5f6f323eff195ed0d505
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-23 19:19:38 -0700

    delete commented out code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[google_oauth2]
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_many :rooms, -> { order('created_at DESC') }
   has_many :tasks, through: :rooms
   has_many :user_tasks
   has_many :helped_tasks, through: :user_tasks, source: :task
   has_many :helpers, through: :tasks
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
     user ||= User.create(first_name: data['first_name'],
                          last_name: data['last_name'],
                          email: data['email'],
