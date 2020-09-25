885bcfe55465ecd2fc7794b76ee7a52539324ed9
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-24 18:29:38 -0700

    add logic to handle search params

--- app/controllers/needs_help_controller.rb
+++ app/controllers/needs_help_controller.rb
@@ -1,6 +1,12 @@
 class NeedsHelpController < ApplicationController
   def index
-    @tasks = Task.needs_help
-    @usertask = UserTask.find_by(id: params[:id])
+    if params[:name]
+      @tasks = Task.needs_help
+      @search = @tasks.where('name LIKE ?', "%#{params[:name]}%")
+    else
+      @tasks = Task.needs_help
+
+      @usertask = UserTask.find_by(id: params[:id])
+    end
   end
 end
